8fe7dd6509c9b3df7a402d15b40d614454a35df7
Daniel Sinai <daniel.sinai@mail.huji.ac.il>
2022-05-07 22:42:45 +0300

    Added to the class login function, setup function and choose library function

--- bot.py
+++ bot.py
@@ -1,7 +1,16 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
+from datetime import date
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def setup():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    return driver
 
 
 class RoomBot:
@@ -10,6 +19,27 @@ class RoomBot:
         self.user_password = user_password
         self.ID_list = ID_list
         self.start_day = 0
+
+    def login(self, driver):
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'pills-email-tab'))).click()
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'username'))).send_keys(self.user_email)  # add email instead of empty string
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'password'))).send_keys(self.user_password)  # add password instead of empty string
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(., "Enter")]'))).click()
+
+    def choose_library(self):
+        driver = setup()
+        driver.get('https://www.huji.ac.il/rooms/')
+        driver.maximize_window()
+
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'open_pop'))).click()
+
+        self.login(driver)
+
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//label[text()='בית הספר להנדסה ולמדעי המחשב']"))).click()
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'next'))).click()
+
+        time.sleep(20)
+        driver.close()
 
     def book_room(self):
         options = webdriver.ChromeOptions()
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from bot import RoomBot
 
-#in this function we will use our bot
-def main():
-    pass
 
-if __name__ == "main":
+def main():
+    id_list = [209462589, 207377648, 209194976, 20895126, 207838277, 20848857,
+               315858548, 208309575, 315315523, 316413715]
+    my_email = ""  # add your email
+    my_password = ""  # add your password
+    rb = RoomBot(id_list, my_email, my_password)
+    rb.choose_library()
+
+
+if __name__ == "__main__":
     main()
