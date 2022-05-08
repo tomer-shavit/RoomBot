3b580c62a39f11ec19136ebd64002cf73158080b
Daniel Sinai <daniel.sinai@mail.huji.ac.il>
2022-05-08 07:45:36 +0300

    Added to the class login function, setup function and choose library function

--- bot.py
+++ bot.py
@@ -14,55 +14,42 @@ def setup():
 
 
 class RoomBot:
-    def __init__(self, ID_list, user_email, user_password):
+    def __init__(self, id_list, user_email, user_password, day):
+        self.driver = setup()
         self.user_email = user_email
         self.user_password = user_password
-        self.ID_list = ID_list
-        self.start_day = 0
+        self.ID_list = id_list
+        self.day = day
 
-    def login(self, driver):
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'pills-email-tab'))).click()
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'username'))).send_keys(self.user_email)  # add email instead of empty string
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'password'))).send_keys(self.user_password)  # add password instead of empty string
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(., "Enter")]'))).click()
+    def login(self):
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'pills-email-tab'))).click()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'username'))).send_keys(self.user_email)  # add email instead of empty string
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'password'))).send_keys(self.user_password)  # add password instead of empty string
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[contains(., "Enter")]'))).click()
+
+    def open_website(self):
+        self.driver.get('https://www.huji.ac.il/rooms/')
+        self.driver.maximize_window()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'open_pop'))).click()
 
     def choose_library(self):
-        driver = setup()
-        driver.get('https://www.huji.ac.il/rooms/')
-        driver.maximize_window()
+        self.open_website()
+        self.login()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//label[text()='בית הספר להנדסה ולמדעי המחשב']"))).click()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, 'next'))).click()
 
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'open_pop'))).click()
+    def choose_date(self):
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@day='12']"))).click()
 
-        self.login(driver)
+    def choose_rooms(self):
+        # WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//checkbox[@value='14,121']"))).click()
+        # WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//checkbox[@value='15,121']"))).click()
+        # WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//checkbox[@value='16,121']"))).click()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@text()='המשך']"))).click()
 
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//label[text()='בית הספר להנדסה ולמדעי המחשב']"))).click()
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'next'))).click()
-
-        time.sleep(20)
-        driver.close()
-
-    def book_room(self):
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("detach", True)
-        self.start_day = input("Please write the start date:\n")
-        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-        # driver = webdriver.Chrome(self.driver_address)
-        driver.get('https://www.huji.ac.il/rooms/')
-        library_check = driver.find_element(By.ID, "Computer")
-        library_check.click()
-        time.sleep(2000)
-        library_next = driver.find_element(By.ID, "next")
-        print(library_next.text)
-        library_next.click()
-        time.sleep(1000)
-        date_button = driver.find_element(By.XPATH, f"//button[@day,'{self.start_day}')]")
-        date_button.click()
-
-
-
-
-
-
-
-
+    def run(self):
+        self.choose_library()
+        self.choose_date()
+        # self.choose_rooms()
+        time.sleep(100)
 
--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from bot import RoomBot
 def main():
     id_list = [209462589, 207377648, 209194976, 20895126, 207838277, 20848857,
                315858548, 208309575, 315315523, 316413715]
-    my_email = ""  # add your email
-    my_password = ""  # add your password
-    rb = RoomBot(id_list, my_email, my_password)
-    rb.choose_library()
+    my_email = input("Enter your email address: \n")  # add your email
+    my_password = input("Enter your password: \n")  # add your password
+    rb = RoomBot(id_list, my_email, my_password, 12)
+    rb.run()
 
 
 if __name__ == "__main__":
